8216a92547aca12b2cdde6cd962598784493915e
sumanthtony <sumanthbattagiri99@gmail.com>
2026-04-03 13:43:26 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "dev-subnet-public-1" {
     vpc_id = "${aws_vpc.dev-vpc.id}" //Attaching the VPC to subnet 
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true" //it makes this a public subnet
-    availability_zone = "us-west-2a"
+    availability_zone = "ap-south-1a"
     tags = {
         Name = "dev-subnet-public-1"
     }
